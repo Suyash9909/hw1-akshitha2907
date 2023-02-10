3df4a25469b26daaf63356a959a52dc8a48fd720
akshitha2907 <123828684+akshitha2907@users.noreply.github.com>
2023-02-09 23:48:55 -0500

    changed last edit

--- answer.py
+++ answer.py
@@ -63,7 +63,7 @@ def list_1D():
     l=[2,3,4,1,5,6,9,10,15,12,13,-2,-6,0,0]
 
     # Inplace sort list l (use .sort() ).
-
+    l.sort()
 
     # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
 
